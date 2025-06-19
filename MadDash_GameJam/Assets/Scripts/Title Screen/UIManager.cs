a7dbe53b3305215260bd4b6a882fd2c7bd2e0d93
tableleg2000 <charliecparker@yahoo.co.uk>
2025-06-19 17:05:43 +0100

    Worked On Menu and started to add sound

--- MadDash_GameJam/Assets/Scripts/Title Screen/UIManager.cs
+++ MadDash_GameJam/Assets/Scripts/Title Screen/UIManager.cs
@@ -6,7 +6,23 @@ using UnityEngine.SceneManagement;
 public class UIManager : MonoBehaviour
 {
     public Transform car;
+    public Transform title;
+    public AudioSource playAudio, quitAudio;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public void playButton()
+    {
+        playAudio.Play();
+    }
+
+    public void quitButton()
+    {
+        quitAudio.Play();
+    }
+
+
+
     void Start()
     {
         StartCoroutine(carMovesIn());
@@ -19,9 +35,11 @@ public class UIManager : MonoBehaviour
 
     IEnumerator carMovesIn()
     {
-        car.DOMove(new Vector3(3, -3.35f, 0), 1);
+        title.DOMove(new Vector3(Screen.width / 2 - (Screen.width * 0.2f), Screen.height / 2, 0), 1);
+        car.DOMove(new Vector3(3, -3.5f, 0), 1);
         yield return new WaitForSeconds(1f);
-        car.DOMove(new Vector3(-4, -3.35f, 0), 1);
+        title.DOMove(new Vector3(Screen.width / 2, Screen.height / 2, 0), 1);
+        car.DOMove(new Vector3(-4, -3.5f, 0), 1);
 
 
 
@@ -29,10 +47,18 @@ public class UIManager : MonoBehaviour
 
     IEnumerator gameStart()
     {
-        car.DOMove(new Vector3(-2, -3.35f, 0), 1);
+        car.DOMove(new Vector3(-4, -3.5f, 0), 1);
+        title.DOMove(new Vector3(Screen.width / 2 + (Screen.width * 0.2f), Screen.height / 2, 0), 1);
         yield return new WaitForSeconds(1f);
-        car.DOMove(new Vector3(15, -3.35f, 0), 1.5f);
+        car.DOMove(new Vector3(15, -3.5f, 0), 1.5f);
+        title.DOMove(new Vector3(-(Screen.width * 0.2f), Screen.height / 2, 0), 1);
         yield return new WaitForSeconds(1.5f);
         SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game is Exiting");
     }
 }
